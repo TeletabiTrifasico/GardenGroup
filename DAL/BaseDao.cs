48697d605ff328c27bf5a23ca8297d59507aedb2
XaFF-XaFF <xawery.czapczynski@gmail.com>
2024-11-05 23:25:55 +0100

    Clean up

--- DAL/BaseDao.cs
+++ DAL/BaseDao.cs
@@ -1,11 +1,5 @@
 ﻿using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Model;
 
 namespace DAL;
 
